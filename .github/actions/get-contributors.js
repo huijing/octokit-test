fea92c47ec90160b63268ce4585eaef19507b9cd
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-07-11 18:46:21 +0800

    headers fail

--- .github/actions/get-contributors.js
+++ .github/actions/get-contributors.js
@@ -8,9 +8,6 @@ async function test() {
     {
       owner: "huijing",
       repo: "tools",
-      headers: {
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
     }
   );
   console.log(data);
