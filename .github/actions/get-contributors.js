53ceb74269df523cff0756c3e7f54744dabeb31e
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2023-07-11 18:42:31 +0800

    async problems

--- .github/actions/get-contributors.js
+++ .github/actions/get-contributors.js
@@ -2,15 +2,18 @@ const { Octokit } = require("@octokit/action");
 
 const octokit = new Octokit();
 
-const { data } = await octokit.request(
-  "GET /repos/huijing/tools/collaborators",
-  {
-    owner: "huijing",
-    repo: "tools",
-    headers: {
-      "X-GitHub-Api-Version": "2022-11-28",
-    },
-  }
-);
+async function test() {
+  const { data } = await octokit.request(
+    "GET /repos/huijing/tools/collaborators",
+    {
+      owner: "huijing",
+      repo: "tools",
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    }
+  );
+  console.log(data);
+}
 
-console.log(data);
+test();
